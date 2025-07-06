2db7d12c5790163bbb355a64c099b66af54c6784
Boruebork <markter2011@gmail.com>
2025-07-06 19:08:51 +0300

    Предметы

---
+++ src/main/java/net/legendarydev/legendarymod/item/ModItems.java
@@ -0,0 +1,18 @@
+package net.legendarydev.legendarymod.item;
+
+import net.legendarydev.legendarymod.LegendaryMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(LegendaryMod.MOD_ID);
+
+    public static final DeferredItem<Item> TITANIUM_INGOT = ITEMS.register("titanium_ingot",
+            ()-> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
