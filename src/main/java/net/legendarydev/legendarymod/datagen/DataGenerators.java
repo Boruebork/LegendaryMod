798f77e8b8f40bbbedb37ff221b8b1e8fa2c77ca
Boruebork <markter2011@gmail.com>
2026-08-04 16:34:42 +0300

    Дропы и теги блоков

--- src/main/java/net/legendarydev/legendarymod/datagen/DataGenerators.java
+++ src/main/java/net/legendarydev/legendarymod/datagen/DataGenerators.java
@@ -1,12 +1,20 @@
 package net.legendarydev.legendarymod.datagen;
 
 import net.legendarydev.legendarymod.LegendaryMod;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 @EventBusSubscriber(modid = LegendaryMod.MOD_ID)
 public class DataGenerators {
@@ -16,8 +24,18 @@ public class DataGenerators {
         DataGenerator generator = event.getGenerator();
         PackOutput output = generator.getPackOutput();
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
         generator.addProvider(event.includeClient(), new ModItemModelProvider(output, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(output, existingFileHelper));
+
+        generator.addProvider(event.includeServer(), new ModBlockTagProvider(output, lookupProvider, existingFileHelper));
+        generator.addProvider(event.includeServer(), new LootTableProvider(output, Set.of(),
+                List.of(new LootTableProvider.SubProviderEntry(
+                        ModBlockLootTableProvider::new,
+                        LootContextParamSets.BLOCK
+                        )
+                ), lookupProvider)
+        );
     }
 }
---
+++ src/main/java/net/legendarydev/legendarymod/datagen/ModBlockLootTableProvider.java
@@ -0,0 +1,27 @@
+package net.legendarydev.legendarymod.datagen;
+
+import net.legendarydev.legendarymod.block.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+import java.util.Set;
+
+public class ModBlockLootTableProvider extends BlockLootSubProvider {
+    protected ModBlockLootTableProvider(HolderLookup.Provider registries) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
+    }
+
+    @Override
+    protected void generate() {
+        dropSelf(ModBlocks.TITANIUM_BLOCK.get());
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(b -> (Block) b.value()).toList();
+    }
+}
